920db04783c016a7106aaa625efaef1ebd721354
David Nicholson <me@djnicholson.com>
2021-03-26 19:06:22 -0700

    Quick start 4

--- src/data/quickStarts.ts
+++ src/data/quickStarts.ts
@@ -7,6 +7,7 @@ const quickStarts: QuickStart[] = [
   { number: 1, title: "Set up your development environment" },
   { number: 2, title: "Create a private Neo blockchain" },
   { number: 3, title: "Creating wallets and transferring assets" },
+  { number: 4, title: "Create and deploy a smart contract" },
 ];
 
 export { quickStarts, QuickStart };
