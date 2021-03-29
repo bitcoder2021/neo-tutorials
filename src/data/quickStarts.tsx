cf871312b769a3fe2736f38098156502a928445e
David Nicholson <me@djnicholson.com>
2021-03-29 16:26:03 -0700

    add notes below videos

---
+++ src/data/quickStarts.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+type QuickStart = {
+  number: number;
+  title: string;
+  notes?: JSX.Element[];
+};
+
+const quickStarts: QuickStart[] = [
+  {
+    number: 1,
+    title: "Set up your development environment",
+    notes: [
+      <p>
+        The .NET SDK can be downloaded from:
+        <br />
+        <a href="https://dotnet.microsoft.com/download">
+          https://dotnet.microsoft.com/download
+        </a>
+      </p>,
+      <p>
+        Visual Studio Code can be downloaded from:
+        <br />
+        <a href="https://code.visualstudio.com/">
+          https://code.visualstudio.com/
+        </a>
+      </p>,
+      <p>
+        This is the URL to the official Neo Blockchain Toolkit extension for
+        Visual Studio Code:
+        <br />
+        <a href="https://marketplace.visualstudio.com/items?itemName=ngd-seattle.neo-blockchain-toolkit">
+          https://marketplace.visualstudio.com/items?itemName=ngd-seattle.neo-blockchain-toolkit
+        </a>
+      </p>,
+    ],
+  },
+  { number: 2, title: "Create a private Neo blockchain" },
+  { number: 3, title: "Creating wallets and transferring assets" },
+  {
+    number: 4,
+    title: "Create and deploy a smart contract",
+    notes: [
+      <p>
+        This is the URL to the official C# extension for Visual Studio Code:
+        <br />
+        <a href="https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp">
+          https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp
+        </a>
+      </p>,
+      <p>
+        The command to install the Neon complier is:{" "}
+        <code>
+          <pre>dotnet tool install -g neo.neon --version 3.0.0-rc1</pre>
+        </code>
+      </p>,
+    ],
+  },
+  {
+    number: 5,
+    title: "Debugging smart contracts",
+    notes: [
+      <p>
+        The launch.json schema for debugging Neo Smart Contracts is documented
+        here:
+        <br />
+        <a href="https://github.com/neo-project/neo-debugger/blob/master/docs/debug-config-reference.md">
+          https://github.com/neo-project/neo-debugger/blob/master/docs/debug-config-reference.md
+        </a>
+      </p>,
+    ],
+  },
+];
+
+export { quickStarts, QuickStart };
--- src/pages/QuickStartPage.tsx
+++ src/pages/QuickStartPage.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import H1 from "../components/H1";
 import H2 from "../components/H2";
 import { QuickStart } from "../data/quickStarts";
 import QuickStartList from "../components/QuickStartList";
+import TutorialList from "../components/TutorialList";
 
 type Props = {
   quickStart: QuickStart;
@@ -24,8 +25,20 @@ export default function QuickStartPage({ quickStart }: Props) {
           <a href={videoHref}>click here to download the video file</a>.
         </video>
       </div>
+      {!!quickStart.notes?.length && (
+        <>
+          <H2>Notes:</H2>
+          <div style={{ marginLeft: 40 }}>
+            {quickStart.notes.map((_, i) => (
+              <Fragment key={i}>{_}</Fragment>
+            ))}
+          </div>
+        </>
+      )}
       <H2>All Quick Starts:</H2>
       <QuickStartList />
+      <H2>Full Tutorials:</H2>
+      <TutorialList />
     </>
   );
 }
