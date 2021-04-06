55f9202ab35e033e029ec99e51d98f59b97aa938
David Nicholson <me@djnicholson.com>
2021-04-06 12:52:34 -0700

    "Commands used in this video"

--- src/data/quickStarts.tsx
+++ src/data/quickStarts.tsx
@@ -46,6 +46,14 @@ const quickStarts: QuickStart[] = [
         <br />
         <code>$ sudo apt install libsnappy-dev libc6-dev -y</code>
       </p>,
+      <p>Commands used in this video:</p>,
+      <p style={{ marginLeft: 30 }}>
+        <em>Checking for .NET SDK installation:</em>
+        <br />
+        <code>$ dotnet --version</code>
+        <br />
+        (Check for a value greater than 5.)
+      </p>,
     ],
   },
   { number: 2, title: "Working with the Private Net" },
@@ -61,9 +69,14 @@ const quickStarts: QuickStart[] = [
           https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp
         </a>
       </p>,
-      <p>
-        The command to install the Neon complier is:{" "}
-        <code>dotnet tool install -g neo.neon --version 3.0.0-rc1</code>
+      <p>Commands used in this video:</p>,
+      <p style={{ marginLeft: 30 }}>
+        <em>Installing the Neon compiler:</em>
+        <br />
+        <code>$ dotnet tool install -g neo.neon --version 3.0.0-rc1</code>
+        <br />
+        (The --version parameter will no longer be required after the final N3
+        release.)
       </p>,
     ],
   },
